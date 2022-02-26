3c8a5a5250621a157cd25d86ffb3cdd9058c415c
Curtis <curtistgo@gmail.com>
2022-02-26 18:54:05 -0500

    rendering our first two templates

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blogpost import views as blogpost_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blogpost_views.index),
+    path('blogposts/', blogpost_views.blogposts)
 ]
