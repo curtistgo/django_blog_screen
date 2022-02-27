ada50a1b728253529df2589e45595f2eb86dc3c3
Curtis <curtistgo@gmail.com>
2022-02-26 19:56:42 -0500

    performed makemigration and migrate

--- blogpost/models.py
+++ blogpost/models.py
@@ -1,3 +1,13 @@
+import imp
 from django.db import models
+import datetime
 
 # Create your models here.
+class Blogpost(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    publish_date = models.DateField(default=datetime.date.today)
+
+
+    
